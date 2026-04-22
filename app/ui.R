61c57e29ca94873cc28614f95a68c85fa26888b9
Lisa Nicvert <lisa.nicvert@fondationbiodiversite.fr>
2026-04-22 14:44:23 +0200

    added info text

--- app/ui.R
+++ app/ui.R
@@ -1,6 +1,9 @@
 fluidPage(
        # run bslib::bs_theme_preview() to customize
        # theme = bs_theme(preset = "cosmo"),
+       tags$head(
+         tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+         ),
 
        # Application title
        titlePanel("Distribution of odonates across Europe. DRAGON, FRB-CESAB"),
@@ -12,23 +15,38 @@ fluidPage(
                             column(
                                    6,
                                    selectInput(
-                                          "spe",
-                                          "Species:",
-                                          choices = sp_choices,
-                                          selected = sp_choices[1],
-                                          multiple = FALSE
-                                   ),
+                                     "spe",
+                                     "Species",
+                                     choices = sp_choices,
+                                     selected = sp_choices[1],
+                                     multiple = FALSE
+                                     ),
                                    br()
                             ),
                             column(
                                    3,
-                                   selectInput(
-                                          "map",
-                                          "Map:",
-                                          choices = map_choices,
-                                          selected = map_choices[1],
-                                          multiple = FALSE
-                                   )
+                                   htmltools::tags$span(
+                                     htmltools::tags$div(
+                                       class = "inline",
+                                       selectInput(
+                                              "map",
+                                              "Map",
+                                              choices = map_choices,
+                                              selected = map_choices[1],
+                                              multiple = FALSE
+                                       )
+                                     ),
+                                     htmltools::tags$div(
+                                       class = "mytooltip",
+                                       icon("circle-info"),
+                                       htmltools::tags$p(class = "mytooltiptext",
+                                                         "mean occupancy: species mean occupancy in 2000-2024; 
+                                                          occupancy slope: linear slope of occupancy between 2000 and 2024; 
+                                                          dynamic occupancy: occupancy values for each year betweem 2000 and 2024 (choose); 
+                                                          site deviation (mean): deviation from the mean occupancy of similar sites; 
+                                                          site deviation (mean): deviation of sites from the global temporal trend" )
+                                       )
+                                     )
                             ),
                             column(
                                    3,
@@ -44,32 +62,64 @@ fluidPage(
                                    navset_card_tab(
                                           full_screen = TRUE,
                                           nav_panel(
-                                                 "Country",
+                                                 title = htmltools::span(
+                                                   "Country",
+                                                   htmltools::tags$div(
+                                                     class = "mytooltip",
+                                                     icon("circle-info"),
+                                                     htmltools::tags$p(class = "mytooltiptext",
+                                                                       "Occupancy trend for each country (average across all cells)" )
+                                                   )
+                                                 ),
                                                  plotly::plotlyOutput(
                                                         'countryts',
                                                         height = "600px"
                                                  )
                                           ),
                                           nav_panel(
-                                                 "Psi_time",
-                                                 plotly::plotlyOutput(
-                                                        'psits',
-                                                        height = "600px"
-                                                 )
-                                          ),
-                                          nav_panel(
-                                                 "Phenology",
+                                            title = htmltools::span(
+                                              "Phenology",
+                                              htmltools::tags$div(
+                                                class = "mytooltip",
+                                                icon("circle-info"),
+                                                htmltools::tags$p(class = "mytooltiptext",
+                                                                  "Inferred phenology (variation in detection probability across the year)" )
+                                              )
+                                            ),
                                                  plotly::plotlyOutput(
                                                         'phenots',
                                                         height = "600px"
                                                  )
                                           ),
                                           nav_panel(
-                                                 "Drivers",
+                                            title = htmltools::span(
+                                              "Environmental variables",
+                                              htmltools::tags$div(
+                                                class = "mytooltip",
+                                                icon("circle-info"),
+                                                htmltools::tags$p(class = "mytooltiptext",
+                                                                  "Estimated influence on occupancy (logit scale)" )
+                                              )
+                                            ),
                                                  plotly::plotlyOutput(
                                                         'envplot',
                                                         height = "600px"
                                                  )
+                                          ),
+                                          nav_panel(
+                                            title = htmltools::span(
+                                              "Time coefficients",
+                                              htmltools::tags$div(
+                                                class = "mytooltip",
+                                                icon("circle-info"),
+                                                htmltools::tags$p(class = "mytooltiptext",
+                                                                  "Deviation from the mean occupancy for each year (logit scale)" )
+                                              )
+                                            ),
+                                            plotly::plotlyOutput(
+                                              'psits',
+                                              height = "600px"
+                                            )
                                           )
                                    )
                             ),
